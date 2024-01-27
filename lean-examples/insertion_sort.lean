9bc5c8fa9df0b86d55c4f58891a692e0a30807eb
Evan Laufer <evan.m.laufer@gmail.com>
2024-01-26 17:01:31 -0800

    cleanup

--- lean-examples/insertion_sort.lean
+++ lean-examples/insertion_sort.lean
@@ -1,6 +1,6 @@
 def sub_eff : ℕ → ℕ → ℕ := 
   λ n m, @nat.rec (λ _, ℕ) n (λ mp prev, @nat.rec (λ _, ℕ) nat.zero (λ mpp _, mpp) prev) m
-
+--
 def is_zero : ℕ -> bool :=
   λ n, @nat.rec (λ _, bool) true (λ np prev, false) n
 
@@ -12,8 +12,8 @@ def le_eff : ℕ → ℕ → bool :=
 
 axiom is_eq_refl (x : ℕ) : is_eq x x = tt
 
-def mem : ℕ -> list ℕ -> bool :=
-  λ x l, list.rec_on l ff (λ hd tl prev, prev || (is_eq hd x))
+def mem : ℕ -> list ℕ -> Prop :=
+  λ x l, list.rec_on l false (λ hd tl prev, prev ∨ (is_eq hd x = tt))
 
 def bor_eq_split {a b : bool} (h: a || b = tt): a = tt ∨ b = tt :=
 begin
@@ -43,13 +43,17 @@ begin
   trivial,
 end
 
-#print bool.ff_ne_tt
-#print bool.no_confusion
+def ff_ne_tt : ff = tt -> false :=
+begin
+  intro h,
+  trivial,
+end
+
 def bor_eq_combine {a b c: bool} (h : a = tt ∨ b = tt ∨ c = tt): a || b || c = tt :=
 begin
   cases a,
   cases b,
-  exact (or.elim h (λ p, absurd p bool.ff_ne_tt) (λ p, or.elim p (λ p, absurd p bool.ff_ne_tt) (bor_eq_left))),
+  exact (or.elim h (λ p, absurd p ff_ne_tt) (λ p, or.elim p (λ p, absurd p ff_ne_tt) (bor_eq_left))),
   exact bor_eq_right (eq.refl tt),
   exact bor_eq_right (eq.refl tt),
 end
@@ -57,40 +61,65 @@ end
 def bor_eq_combine2 {a b: bool} (h : a = tt ∨ b = tt): a || b = tt :=
 begin
   cases a,
-  exact (or.elim h (λ p, absurd p bool.ff_ne_tt) (λ p, bor_eq_left p)),
+  exact (or.elim h (λ p, absurd p ff_ne_tt) (λ p, bor_eq_left p)),
   exact bor_eq_right (eq.refl tt),
 end
 
-def mem_head : ∀ (hd : ℕ) (tl : list ℕ), mem hd (hd :: tl) = tt:=
-  λ hd tl, bor_eq_left (is_eq_refl hd)
+def mem_head : ∀ (hd : ℕ) (tl : list ℕ), mem hd (hd :: tl) :=
+  λ hd tl, or.intro_right _ (is_eq_refl hd)
 
-def mem_cons : ∀ {x : ℕ } (hd : ℕ) {tl : list ℕ} (h : mem x tl = tt), mem x (hd :: tl) = tt :=
-  λ x hd tl h, (bor_eq_right h)
+def mem_cons : ∀ {x : ℕ } (hd : ℕ) {tl : list ℕ} (h : mem x tl), mem x (hd :: tl) :=
+  λ x hd tl h, or.intro_left _ h
+
+theorem or_comm_ : ∀ {a b: Prop}, a ∨ b -> b ∨ a :=
+  λ a b, or.swap
 
 def or_comm_helper : ∀ {p q r : Prop} (h : p ∨ q ∨ r), (p ∨ r ∨ q) :=
 begin
   intros,
-  exact or.elim h (or.intro_left (r ∨ q)) (λ qr, or.intro_right p (or.comm.mp qr)),
+  exact or.elim h (or.intro_left (r ∨ q)) (λ qr, or.intro_right p (or_comm_ qr)),
 end
 
-def mem_reorder : ∀ {x y z: ℕ} {tl : list ℕ} (h : mem x (y :: z :: tl) = tt), mem x (z :: y :: tl) = tt :=
+theorem or_assoc_forward : ∀ {a b c : Prop}, (a ∨ b) ∨ c -> a ∨ b ∨ c :=
+begin
+  intros a b c h,
+  induction h,
+  apply or.elim h,
+  intro ha,
+  exact or.intro_left _ ha,
+  intro hb,
+  exact or.intro_right _ (or.intro_left _ hb),
+  exact or.intro_right _ (or.intro_right _ h),
+end
+
+theorem or_assoc_backward : ∀ {a b c : Prop}, a ∨ b ∨ c -> (a ∨ b) ∨ c :=
+begin
+  intros a b c h,
+  induction h,
+  exact or.intro_left _ (or.intro_left _ h),
+  induction h,
+  exact or.intro_left _ (or.intro_right _ h),
+  exact or.intro_right _ h, 
+end
+
+def mem_reorder : ∀ {x y z: ℕ} {tl : list ℕ} (h : mem x (y :: z :: tl)), mem x (z :: y :: tl) :=
 begin
   intros,
-  let h1 := bor_eq_split h,
-  let h2 := or.elim h1 (λ p, or.intro_left (is_eq y x = tt) (bor_eq_split p)) (λ p, or.intro_right _ p),
-  let h3 := or.assoc.mp h2,
+  let h1 := h,
+  let h2 := or.elim h1 (λ p, or.intro_left (is_eq y x = tt) ( p)) (λ p, or.intro_right _ p),
+  let h3 := or_assoc_forward h2,
   let h4 := or_comm_helper h3,
-  let h5 := bor_eq_combine h4,
+  let h5 := or_assoc_backward h4,
   exact h5,
 end
 
 def subset : list ℕ -> list ℕ -> Prop :=
-  λ s l, list.rec_on s true (λ hd tl prev, (mem hd l = tt) ∧ prev)
+  λ s l, list.rec_on s true (λ hd tl prev, (mem hd l) ∧ prev)
 
 def subset_nil : ∀ (l : list ℕ), subset [] l :=
   λ l, trivial
 
-def subset_cons : ∀ {s l : list ℕ} {hd : ℕ} (p : mem hd l = tt) (h: subset s l), subset (hd :: s) l := 
+def subset_cons : ∀ {s l : list ℕ} {hd : ℕ} (p : mem hd l) (h: subset s l), subset (hd :: s) l := 
   λ s l hd p h, and.intro p h
 
 def subset_cons_list : ∀ {s l : list ℕ} (hd : ℕ) (h: subset s l), subset s (hd :: l) :=
@@ -120,12 +149,31 @@ begin
   exact and.elim_right h,
 end
 
+theorem and_assoc_forward : ∀ {a b c : Prop}, (a ∧ b) ∧ c -> a ∧ b ∧ c :=
+begin
+  intros a b c h,
+  induction h,
+  induction h_left,
+  exact and.intro h_left_left (and.intro h_left_right h_right)
+end
+
+theorem and_assoc_backward : ∀ {a b c : Prop}, a ∧ b ∧ c ->  (a ∧ b) ∧ c  :=
+begin
+  intros a b c h,
+  induction h,
+  induction h_right,
+  exact and.intro (and.intro h_left h_right_left) h_right_right
+end
+
+theorem and_comm_ : ∀ {a b: Prop}, a ∧ b -> b ∧ a :=
+  λ a b, and.swap 
+
 def subset_reorder_s : ∀ {tl l : list ℕ} {x y : ℕ} (h : subset (x :: y :: tl) l), subset (y :: x :: tl) l :=
 begin
   intros,
-  let h1 := and.elim_left (and.assoc.mpr h),
-  let h2 := and.elim_right (and.assoc.mpr h),
-  exact and.assoc.mp (and.intro (and.comm.mp h1) h2),
+  let h1 := and.elim_left (and_assoc_backward h),
+  let h2 := and.elim_right (and_assoc_backward h),
+  exact and_assoc_forward (and.intro (and_comm_ h1) h2),
 end
 
 def subset_eq : ∀ (l : list ℕ), subset l l :=
@@ -171,7 +219,7 @@ begin
 end
 
 def sorted : list ℕ -> Prop :=
-  λ l, list.rec_on l true (λ hd tl prev, list.rec_on tl true (λ hd_tl tl_tl prev_tl, le_eff hd hd_tl ∧ prev) )
+  λ l, list.rec_on l true (λ hd tl prev, list.rec_on tl true (λ hd_tl tl_tl prev_tl, le_eff hd hd_tl = tt ∧ prev) )
 def sorted_nil : sorted [] := trivial
 def sorted_singleton (x : ℕ) : sorted [x] := trivial
 def sorted_cons_le (x : ℕ) (hd : ℕ) (tl: list ℕ) (h: sorted (hd :: tl)) (h2: le_eff x hd = tt) : sorted (x :: hd :: tl) :=
@@ -205,31 +253,42 @@ begin
 end
 
 #print or.elim
-def mem_insert_sorted {x : ℕ} {l : list ℕ} (y : ℕ ) (h : mem x l = tt) : mem x (insert_sorted y l) = tt :=
+def mem_insert_sorted {x : ℕ} {l : list ℕ} (y : ℕ ) (h : mem x l) : mem x (insert_sorted y l) :=
 begin
   induction l,
   dunfold mem at h,
-  exact false.elim (bool.ff_ne_tt h),
+  exact false.elim (h),
   dunfold insert_sorted,
   cases (decidable_bool_eq (le_eff y l_hd)),
   let h_1 := not_eq_ff_eq_tt h_1,
-  exact @eq.subst bool (λ b, mem x (bool.rec (l_hd :: list.rec [y] (λ (hd : ℕ) (tl prev : list ℕ), bool.rec (hd :: prev) (y :: hd :: tl) (le_eff y hd)) l_tl) (y :: l_hd :: l_tl) b) = tt) tt (le_eff y l_hd) (flip_eq h_1) ((mem_cons y h)),
+  let h2 := (mem_cons y h),
+  exact @eq.subst bool (λ b, mem x (bool.rec (l_hd :: list.rec [y] (λ (hd : ℕ) (tl prev : list ℕ), bool.rec (hd :: prev) (y :: hd :: tl) (le_eff y hd)) l_tl) (y :: l_hd :: l_tl) b)) tt (le_eff y l_hd) (flip_eq h_1) h2,
   cases (decidable_bool_eq (is_eq l_hd x)),
-  apply @eq.subst bool (λ b, mem x (bool.rec (l_hd :: list.rec [y] (λ (hd : ℕ) (tl prev : list ℕ), bool.rec (hd :: prev) (y :: hd :: tl) (le_eff y hd)) l_tl) (y :: l_hd :: l_tl) b) = tt) ff (le_eff y l_hd) (flip_eq h_1),
+  apply @eq.subst bool (λ b, mem x (bool.rec (l_hd :: list.rec [y] (λ (hd : ℕ) (tl prev : list ℕ), bool.rec (hd :: prev) (y :: hd :: tl) (le_eff y hd)) l_tl) (y :: l_hd :: l_tl) b)) ff (le_eff y l_hd) (flip_eq h_1),
   dunfold mem,
   let h_2 := not_eq_ff_eq_tt h_2,
-  exact (bor_eq_combine2 (or.intro_right _ h_2)),
-  apply @eq.subst bool (λ b, mem x (bool.rec (l_hd :: list.rec [y] (λ (hd : ℕ) (tl prev : list ℕ), bool.rec (hd :: prev) (y :: hd :: tl) (le_eff y hd)) l_tl) (y :: l_hd :: l_tl) b) = tt) ff (le_eff y l_hd) (flip_eq h_1),
+  exact (or.intro_right _ h_2),
+  apply @eq.subst bool (λ b, mem x (bool.rec (l_hd :: list.rec [y] (λ (hd : ℕ) (tl prev : list ℕ), bool.rec (hd :: prev) (y :: hd :: tl) (le_eff y hd)) l_tl) (y :: l_hd :: l_tl) b)) ff (le_eff y l_hd) (flip_eq h_1),
   dunfold mem,
   dunfold mem at h,
-  let h := or.elim (bor_eq_split h) (λp, p) (λ p, false.elim (bool.ff_ne_tt (eq.trans (flip_eq h_2) p))),
-  let h1 := or.intro_left (is_eq l_hd x) (l_ih h),
-  let h2 := bor_eq_combine2 h1,
+  let h := or.elim (h) (λp, p) (λ p, false.elim (ff_ne_tt (eq.trans (flip_eq h_2) p))),
+  let h1 := or.intro_left (is_eq l_hd x = tt) (l_ih h),
+  let h2 := h1,
   exact h2,
 end
+#print mem_insert_sorted
 
+def and_true_intro (p : Prop): p -> p ∧ true :=
+begin
+  intro h,
+  exact and.intro h true.intro,
+end
 
-#print and_true
+def and_true_elim (p : Prop) : p ∧ true -> p :=
+begin
+  apply and.elim_left,
+end
+
 def subset_insert_sorted (x : ℕ) (s l : list ℕ) (h : subset s l) : subset (x :: s) (insert_sorted x l)  :=
 begin
   induction s,
@@ -239,13 +298,13 @@ begin
   exact (and.intro (mem_head x []) (true.intro)),
   cases (decidable_bool_eq (le_eff x l_hd)),
   let h_1 := not_eq_ff_eq_tt h_1,
-  apply (and_true (mem x (list.rec_on (l_hd :: l_tl) [x] (λ (hd : ℕ) (tl prev : list ℕ), bool.rec_on (le_eff x hd) (hd :: prev) (x :: hd :: tl))) = tt)).mpr,
-  apply @eq.subst bool (λ b, mem x (bool.rec (l_hd :: list.rec [x] (λ (hd : ℕ) (tl prev : list ℕ), bool.rec (hd :: prev) (x :: hd :: tl) (le_eff x hd)) l_tl) (x :: l_hd :: l_tl) b) = tt) tt (le_eff x l_hd) (flip_eq h_1),
+  apply (and_true_intro (mem x (list.rec_on (l_hd :: l_tl) [x] (λ (hd : ℕ) (tl prev : list ℕ), bool.rec_on (le_eff x hd) (hd :: prev) (x :: hd :: tl))))),
+  apply @eq.subst bool (λ b, mem x (bool.rec (l_hd :: list.rec [x] (λ (hd : ℕ) (tl prev : list ℕ), bool.rec (hd :: prev) (x :: hd :: tl) (le_eff x hd)) l_tl) (x :: l_hd :: l_tl) b)) tt (le_eff x l_hd) (flip_eq h_1),
   exact (mem_head x (l_hd :: l_tl)),
-  apply (and_true (mem x (list.rec_on (l_hd :: l_tl) [x] (λ (hd : ℕ) (tl prev : list ℕ), bool.rec_on (le_eff x hd) (hd :: prev) (x :: hd :: tl))) = tt)).mpr,
-  apply @eq.subst bool (λ b, mem x (bool.rec (l_hd :: list.rec [x] (λ (hd : ℕ) (tl prev : list ℕ), bool.rec (hd :: prev) (x :: hd :: tl) (le_eff x hd)) l_tl) (x :: l_hd :: l_tl) b) = tt) ff (le_eff x l_hd) (flip_eq h_1),
+  apply (and_true_intro (mem x (list.rec_on (l_hd :: l_tl) [x] (λ (hd : ℕ) (tl prev : list ℕ), bool.rec_on (le_eff x hd) (hd :: prev) (x :: hd :: tl))))),
+  apply @eq.subst bool (λ b, mem x (bool.rec (l_hd :: list.rec [x] (λ (hd : ℕ) (tl prev : list ℕ), bool.rec (hd :: prev) (x :: hd :: tl) (le_eff x hd)) l_tl) (x :: l_hd :: l_tl) b)) ff (le_eff x l_hd) (flip_eq h_1),
   let h1 := l_ih (subset_nil l_tl),
-  let h1 := (and_true (mem x (list.rec_on l_tl [x] (λ (hd : ℕ) (tl prev : list ℕ), bool.rec_on (le_eff x hd) (hd :: prev) (x :: hd :: tl))) = tt)).mp h1,
+  let h1 := (and_true_elim (mem x (list.rec_on l_tl [x] (λ (hd : ℕ) (tl prev : list ℕ), bool.rec_on (le_eff x hd) (hd :: prev) (x :: hd :: tl))))) h1,
   exact (mem_cons l_hd h1),
   let h2 := s_ih (subset_tail h),
   let h3 := and.elim_left h,
@@ -310,3 +369,6 @@ def subset_sort (l : list ℕ) :  subset (l) (sort l)  :=
 
 def sort_subset (l : list ℕ) : subset (sort l) (l) :=
       (list.rec_on l (trivial) (λ hd tl ih, insert_sorted_subset hd (sort tl) tl ih))
+
+def sort_correct (l : list ℕ) : sorted (sort l) ∧ subset (l) (sort l) ∧ subset (sort l) (l) :=
+  and.intro (sort_sorted l) (and.intro (subset_sort l) (sort_subset l))
