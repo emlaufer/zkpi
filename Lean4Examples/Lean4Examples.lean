44379f0bb0973ef0d5eee63f08f5920723da6fb0
Evan Laufer <evan.m.laufer@gmail.com>
2024-05-06 18:14:43 -0700

    nits

--- Lean4Examples/Lean4Examples.lean
+++ Lean4Examples/Lean4Examples.lean
@@ -3,6 +3,7 @@
 import «Lean4Examples».Basic
 import «Lean4Examples».AndComm
 import «Lean4Examples».DeMorgan
+import «Lean4Examples».DegreeIsEven
 import «Lean4Examples».DedupList
 import «Lean4Examples».InsertionSort
 import «Lean4Examples».Pigeonhole
